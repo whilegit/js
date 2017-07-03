862d3c141bc2934eb8a62652f971019a4a23e1bf
Linzhognren <6215714@qq.com>
2017-07-03 18:10:46 +0800

    jssor.js轮播图宽度自适应

--- static/js/main.js
+++ static/js/main.js
@@ -104,18 +104,38 @@ require(['Chart'], function(Chart){
  * jssor.js 实现轮播图效果
  */
 require(['jssor.slider.min'],function($JssorSlider){
-    var options = {
-        $AutoPlay:1,
-        $Loop:1,
-        $DragOrientation:3,
-        $SlideDuration:500,
-        $ArrowNavigatorOptions:{
-            $Class: $JssorArrowNavigator$,
-            $ChanceToShow:2, 
-            $AutoCenter: 2,
-            $Steps:1
+    var jssor_1_options = {
+        $AutoPlay: 1,
+        $SlideDuration: 800,
+        $SlideEasing: $Jease$.$OutQuint,
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+            },
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$
             }
-    };
-    var jssor_slider1 = new $JssorSlider('slider1_container', options);
+        };
+
+    var jssor_1_slider = new $JssorSlider("jssor_1", jssor_1_options);
+
+    /*#region responsive code begin*/
+    /*you can remove responsive code if you don't want the slider scales while window resizing*/
+    
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 1920);
+            jssor_1_slider.$ScaleWidth(refSize);
+        } else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $Jssor$.$AddEvent(window, "load", ScaleSlider);
+    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+   
+    /*#endregion responsive code end*/
     console.log('jssor.slider.min ok');
 });
+
